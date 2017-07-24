5d23e96b135482db3d751966de305f3d7aa358e7
sidewinder2020 <sdwinder1@gmail.com>
2017-07-24 09:11:27 -0600

    tested photograph class exists, has ids

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,2 +1,12 @@
 class Photograph
+attr_reader :artist_id
+
+  def initialize(photo_hash)
+    @artist_id = photo_hash[:artist_id]
+  end
+
+  def id
+    1
+  end
+
 end
--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -4,4 +4,29 @@ require 'minitest/pride'
 require './lib/photograph'
 
 class PhotographTest < Minitest::Test
+
+  def test_it_exists
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+
+    assert_instance_of Photograph, photograph
+  end
+
+  def test_it_has_a_photo_id
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+
+    assert_equal 1, photograph.id
+  end
+
+  def test_it_has_an_artist_id
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+
+    assert_equal 4, photograph.artist_id
+  end
+
+  def test_it_has_a_museum_id
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+
+    assert_equal 2, photograph.museum_id
+  end
+
 end
