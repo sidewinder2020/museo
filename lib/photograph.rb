a0ce8730db2eaba4ea99fac1e4f343c3e23b5fe8
sidewinder2020 <sdwinder1@gmail.com>
2017-07-24 09:14:43 -0600

    finished photograph class, all tests pass

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,8 +1,11 @@
 class Photograph
-attr_reader :artist_id
+attr_reader :artist_id, :museum_id, :name, :year
 
   def initialize(photo_hash)
     @artist_id = photo_hash[:artist_id]
+    @museum_id = photo_hash[:museum_id]
+    @name = photo_hash[:name]
+    @year = photo_hash[:year]
   end
 
   def id
