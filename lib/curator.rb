36d1a3ec876f1a96a9f6b3a449d50004e5bd081a
sidewinder2020 <sdwinder1@gmail.com>
2017-07-24 09:21:05 -0600

    curator class create instance and add initialize instance variables with empty arrays

--- lib/curator.rb
+++ lib/curator.rb
@@ -1,2 +1,12 @@
+require './lib/photograph'
+
 class Curator
+attr_accessor :artists, :museums, :photographs
+
+  def initialize
+    @artists = []
+    @museums = []
+    @photographs = []
+  end
+
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -4,4 +4,19 @@ require 'minitest/pride'
 require './lib/curator'
 
 class CuratorTest < Minitest::Test
+
+  def test_it_exists
+    curator = Curator.new
+
+    assert_instance_of Curator, curator
+  end
+
+  def test_it_has_artists_museums_photographs
+    curator = Curator.new
+
+    assert_equal [], curator.artists
+    assert_equal [], curator.museums
+    assert_equal [], curator.photographs
+  end
+
 end
